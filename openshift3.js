bad833e4bcf8eebd876960d8b27a889e6437145f
ANAT01 <ANAT01@users.noreply.github.com>
2017-09-03 22:42:11 +0500

    upd

---
+++ openshift3.js
@@ -0,0 +1,30 @@
+var fs = require('fs');
+var tw = require("./boot/boot.js").TiddlyWiki();
+
+var path = process.env.TW_DATA ? process.env.TW_DATA : './';
+
+if (fs.existsSync(path + '/tiddlywiki.info')) {
+    $tw.boot.argv = [
+      path,
+      "--verbose",
+      "--server",
+      "8080",
+      "$:/core/save/all",
+      "text/plain",
+      "text/html",
+      process.env.TW_DATA ? process.env.TW_USER : '',
+      process.env.TW_DATA ? process.env.TW_PASS : '',
+      "0.0.0.0",
+    ];
+}
+else {
+    $tw.boot.argv = [
+      path,
+      "--verbose",
+      "--init",
+      "empty",
+    ];
+}
+
+// Boot the TW5 app
+$tw.boot.boot();
